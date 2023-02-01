e47bfe5e4fbce751570cfb1739b3374c3dd1842d
Bob Developer <bobdeveloperbob@gmail.com>
2023-01-31 22:02:12 -0500

    create jobCard component

---
+++ src/components/jobCard.tsx
@@ -0,0 +1,33 @@
+import React, { ReactEventHandler } from "react";
+type JobCardPropsTypes = {
+  domain: string;
+  startDate: string;
+  endDate: string;
+  position: string;
+  description: string;
+};
+
+function JobCard(props: JobCardPropsTypes) {
+  const { domain, startDate, endDate, position, description } = props;
+  const onClickHandler = () => {
+    console.log(`https://${domain}`);
+  };
+  return (
+    <div className="flex mb-10">
+      {/* <span className="border-2 border-dashed w-3/6">{domain[0]}</span> */}
+      <div className="">
+        <div className="font-bold flex justify-between items-center">
+          <p >
+            <span className="border-b-2 cursor-pointer">{domain}</span>
+            {` | ${position}`}
+          </p>
+          <p className="">{`${startDate} - ${endDate}`}</p>
+        </div>
+
+        <p className="mt-2 pl-3">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default JobCard;
