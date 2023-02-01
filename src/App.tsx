17ced7c0824352af89b3dc0cac2d02e9ba0aae33
Bob Developer <bobdeveloperbob@gmail.com>
2023-01-31 22:04:34 -0500

    add components to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,27 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/header";
+import H2 from "./components/h2";
+import JobCard from "./components/jobCard";
 
 function App() {
   return (
     <div className="App min-h-screen bg-gray-800 text-white flex justify-center flex-col">
-      <header className="pl-2 pr-2 pt-5 flex flex-col fixed top-0 left-0 right-0">
-        <div className="flex self-end items-center">
-          <nav className="hover:underline">#about</nav>
-          <nav className="ml-10 hover:underline">#skills</nav>
-          <nav className="ml-10 hover:underline">#work_expirience</nav>
-          <nav className="ml-10 hover:underline">#projects</nav>
-          <button className="ml-10 mr-5 p-2 pl-5 pr-5 border-2 border-dashed hover:border-solid">
-            RESUME
-          </button>
-        </div>
-        <p className="mt-3 overflow-hidden max-w-full">
-          ========================================================================================================================================================================================================================================================================================================================================================================================================================================================================
-        </p>
-      </header>
+      <Header />
       <div className="container w-9/12 self-center">
         <section className="min-h-screen flex items-center">
           <div className="self-center w-6/12">
-            <h1 className="text-xl font-bold">Hello World! I'm Ivan Kurochkin</h1>
+            <h1 className="text-xl font-bold">
+              Hello World! I'm Ivan Kurochkin
+            </h1>
             <p className="">
               And in this second my code is helping people solve their problems,
               and the owners of that are making money.
             </p>
           </div>
         </section>
-        <section className="flex flex-col justify-center pb-20 mb-20 border-b-4 border-double">
-          <h2 className="border-b-2 pb-1 border-dashed w-fit mb-5 text-lg font-bold">
-            ABOUT
-          </h2>
+        <section className="flex flex-col justify-center pb-20 border-b-4 border-double">
+          <H2 text={"ABOUT"} />
           <p className="mb-1">
             A Middle Full-Stack JavaScript Developer with 2.5 years of
             experience working in startups, specializing in creating massive and
@@ -43,10 +33,8 @@ function App() {
             this direction.
           </p>
         </section>
-        <section className="flex flex-col justify-center mb-20">
-          <h2 className="border-b-2 pb-1 border-dashed w-fit mb-5 text-lg font-bold">
-            SKILLS
-          </h2>
+        <section className="flex flex-col justify-center py-20 border-b-4 border-double">
+          <H2 text={"SKILLS"} />
           <p className="mb-2">
             <span className="font-bold border-b">Programmer Languages:</span>
             {" JavaScript, TypeScript, SQL, Solidity."}
@@ -66,7 +54,7 @@ function App() {
             {" PostgreSQL, MongoDB."}
           </p>
           <p className="mb-5">
-            I am always open to new knowledge and challenges
+            I am always open to new knowledge and challenges.
           </p>
         </section>
       </div>
