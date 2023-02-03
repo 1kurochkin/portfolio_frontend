ee0e1ac60bbbace95960b5806ea9066475e6c530
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-03 16:32:55 -0500

    add firebase & make optimization of code

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,140 @@
-import React from "react";
+import { initializeApp } from "firebase/app";
+import {
+  fetchAndActivate,
+  getAll,
+  getRemoteConfig,
+} from "firebase/remote-config";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Header from "./components/header";
-import H2 from "./components/h2";
-import JobCard from "./components/jobCard";
+import H2 from "./components/h2.component";
+import Header from "./components/header.component";
+import JobCard from "./components/jobCard.component";
+import firebaseConfig from "./configs/firebase.config.json";
+import rcDefault from "./configs/rcDefault.config.json";
+import Footer from "./components/footer.component";
+
+type FirebaseRemoteConfigType = {
+  resume_link: string;
+  welcome: { title: string; subtitle: string };
+  about: { summary: string };
+  skills: {
+    programmer_languages: string;
+    frontend: string;
+    backend: string;
+    databases: string;
+  };
+  work_expirience: Array<{
+    domain: string;
+    position: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+  }>;
+  projects: Array<{ name: string; domain: string; description: string }>;
+  contacts: {
+    email: string;
+    linkedin: string;
+    github: string;
+    resume_link: string;
+  };
+};
 
 function App() {
-  const jobCards = [
+  const [loading, setLoading] = useState<Boolean>(true);
+  const [contentConfig, setContentConfig] =
+    useState<FirebaseRemoteConfigType>();
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const remoteConfig = getRemoteConfig(app);
+    remoteConfig.defaultConfig = rcDefault;
+    remoteConfig.settings.minimumFetchIntervalMillis = 15000;
+    (async () => {
+      await fetchAndActivate(remoteConfig);
+      const remoteConfigValues = getAll(remoteConfig);
+      console.log(remoteConfigValues);
+      for (const key in remoteConfigValues) {
+        setContentConfig((prevState: any) => ({
+          ...prevState,
+          [key]: JSON.parse(remoteConfigValues[key].asString()),
+        }));
+      }
+      setLoading(false);
+    })();
+  }, []);
+
+  const contactLinks = [
     {
-      startDate: "Jul 2021",
-      endDate: "Apr 2022",
-      position: "Backend JavaScript Developer",
-      description:
-        "One of the European IT service providers with 25 employees helped 500+ companies from the whole world to open a legal entity in the Eurozone and take care of tasks of accounting, invoicing, documentation and hr management.",
-      domain: "enty.io",
+      href: `mailto:${contentConfig?.contacts.email}`,
+      text: contentConfig?.contacts.email,
     },
     {
-      startDate: "Jul 2021",
-      endDate: "Apr 2022",
-      position: "Backend JavaScript Developer",
-      description:
-        "One of the European IT service providers with 25 employees helped 500+ companies from the whole world to open a legal entity in the Eurozone and take care of tasks of accounting, invoicing, documentation and hr management.",
-      domain: "enty.io",
+      href: `https://www.linkedin.com/in/${contentConfig?.contacts.github}`,
+      text: `linkedin.com/in/${contentConfig?.contacts.linkedin}`,
     },
     {
-      startDate: "Jul 2021",
-      endDate: "Apr 2022",
-      position: "Backend JavaScript Developer",
-      description:
-        "One of the European IT service providers with 25 employees helped 500+ companies from the whole world to open a legal entity in the Eurozone and take care of tasks of accounting, invoicing, documentation and hr management.",
-      domain: "enty.io",
+      href: `https://github.com/${contentConfig?.contacts.github}`,
+      text: `github.com/${contentConfig?.contacts.github}`,
     },
   ];
+
   return (
-    <div className="App min-h-screen bg-gray-800 text-white flex justify-center flex-col">
-      <Header />
-      <div className="container w-9/12 self-center">
-        <section className="min-h-screen flex items-center">
-          <div className="self-center w-6/12">
-            <h1 className="text-xl font-bold">
-              Hello World! I'm Ivan Kurochkin
-            </h1>
-            <p className="">
-              And in this second my code is helping people solve their problems,
-              and the owners of that are making money.
-            </p>
+    <div className="App min-h-screen bg-gray-800 text-white flex justify-center flex-col text-xl">
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        <>
+          <Header />
+          <div className="container w-9/12 self-center">
+            <section className="flex items-center">
+              <div className="self-center w-3/4">
+                <h1 className="text-3xl font-bold mb-2 text-green-500">
+                  {contentConfig?.welcome.title}
+                </h1>
+                <p className="">{contentConfig?.welcome.subtitle}</p>
+              </div>
+            </section>
+            <section id="about" className="flex flex-col justify-center pb-20 border-b-2 border-dashed">
+              <H2 text={"about"} />
+              <p className="mb-1">{contentConfig?.about.summary}</p>
+            </section>
+            <section id="skills" className="flex flex-col justify-center py-20 border-b-2 border-dashed">
+              <H2 text={"skills"} />
+              {Object.entries(contentConfig?.skills || {}).map(
+                ([key, value]) => (
+                  <p className="mb-5">
+                    <span className="font-bold text-green-500 pb-1">{`${key}:`}</span>
+                    <span className="">{value}</span>
+                  </p>
+                )
+              )}
+            </section>
+            <section id="work_expirience"  className="flex flex-col justify-center py-20 border-b-2 border-dashed">
+              <H2 text={"work_expirience"} />
+              {contentConfig?.work_expirience.map((work: any) => (
+                <JobCard
+                  startDate={work.startDate}
+                  endDate={work.endDate}
+                  position={work.position}
+                  description={work.description}
+                  domain={work.domain}
+                />
+              ))}
+            </section>
+            <section id="projects" className="flex flex-col justify-center py-20">
+              <H2 text={"projects"} />
+              {contentConfig?.projects.map((project: any) => (
+                <JobCard
+                  position={project.name}
+                  description={project.description}
+                  domain={project.domain}
+                />
+              ))}
+            </section>
           </div>
-        </section>
-        <section className="flex flex-col justify-center pb-20 border-b-4 border-double">
-          <H2 text={"ABOUT"} />
-          <p className="mb-1">
-            A Middle Full-Stack JavaScript Developer with 2.5 years of
-            experience working in startups, specializing in creating massive and
-            scalable web apps, mobile apps and server apps from scratch or with
-            an existing codebase.
-          </p>
-          <p className="mb-5">
-            Frontend Development is my destiny, and I'm seeking to develop in
-            this direction.
-          </p>
-        </section>
-        <section className="flex flex-col justify-center py-20 border-b-4 border-double">
-          <H2 text={"SKILLS"} />
-          <p className="mb-2">
-            <span className="font-bold border-b">Programmer Languages:</span>
-            {" JavaScript, TypeScript, SQL, Solidity."}
-          </p>
-          <p className="mb-2">
-            <span className="font-bold border-b">Frontend:</span>
-            {
-              " React, React Native, Redux, RTK, GraphQL, HTML, CSS, SCCS, AntDesign, TailWind, Web3.js."
-            }
-          </p>
-          <p className="mb-2">
-            <span className="font-bold border-b">Backend:</span>
-            {" Node.js, NestJS, Express.js, TypeORM, GraphQL."}
-          </p>
-          <p className="mb-5">
-            <span className="font-bold border-b">Databases:</span>
-            {" PostgreSQL, MongoDB."}
-          </p>
-          <p className="mb-5">
-            I am always open to new knowledge and challenges.
-          </p>
-        </section>
-        <section className="flex flex-col justify-center py-20 border-b-4 border-double">
-          <H2 text={"WORK EXPIRIENCE"} />
-          {jobCards.map((jobCard) => (
-            <JobCard
-              startDate={jobCard.startDate}
-              endDate={jobCard.endDate}
-              position={jobCard.position}
-              description={jobCard.description}
-              domain={jobCard.domain}
-            />
-          ))}
-        </section>
-      </div>
+
+        </>
+      )}
     </div>
   );
 }
