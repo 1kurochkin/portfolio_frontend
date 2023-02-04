78241153913edc6eb1d9818e384cab0b267b03d3
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-04 14:42:23 -0500

    add paragraph.coponent and change text-color classNames in jobCard.component

--- src/components/jobCard.component.tsx
+++ src/components/jobCard.component.tsx
@@ -1,5 +1,5 @@
-import React, { ReactEventHandler } from "react";
 import Link from "./link.component";
+import Paragraph from "./paragraph.component";
 type JobCardPropsTypes = {
   domain: string;
   startDate?: string;
@@ -10,30 +10,24 @@ type JobCardPropsTypes = {
 
 function JobCard(props: JobCardPropsTypes) {
   const { domain, startDate, endDate, position, description } = props;
-  const onClickHandler = () => {
-    console.log(`https://${domain}`);
-  };
   return (
     <div className="flex mb-10">
       <div className="">
         <div className="font-bold flex justify-between items-center">
-          <p className="text-green-500">
-            <Link text={domain} href={`https://${domain}`} />
+          <p className="text-secondary">
+            <Link className="hover:text-primary" value={domain} href={`https://${domain}`} />
             {position && ` | ${position}`}
           </p>
           {startDate && (
-            <span className="text-green-500">{`${startDate} - ${endDate}`}</span>
+            <span className="text-secondary">{`${startDate} - ${endDate}`}</span>
           )}
         </div>
         <p className="mt-2 pl-3 whitespace-pre-line">
-          {description.split("- ").map((text) =>
-            text.length ? (
-              <>
-                <span className="text-green-500 font-bold">- </span>
-                {text}
-              </>
-            ) : null
-          )}
+          {description
+            .split("\n")
+            .map((text) =>
+              text.length ? <Paragraph value={text} className={"mb-5"} /> : null
+            )}
         </p>
       </div>
     </div>
