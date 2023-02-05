b581c4772cda6c1af4ba2e054f2b407196603b83
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-05 12:47:56 -0500

    add responsive styles

--- src/components/h2.component.tsx
+++ src/components/h2.component.tsx
@@ -5,7 +5,7 @@ type H2PropsTypes = {
 function H2(props: H2PropsTypes) {
   const {text} = props;
   return (
-    <h2 className="text-3xl border-b-2 pb-2 border-secondary w-fit mb-10 font-bold ">
+    <h2 className="sm:text-3xl text-3xl border-b-2 pb-2 border-secondary w-fit mb-10 font-bold ">
       <span className="text-secondary">~/</span>
       {text}
     </h2>
