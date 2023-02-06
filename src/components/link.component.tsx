98916f069be245471a6805d421e7db8a43b2a729
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-06 17:57:54 -0500

    update props types

--- src/components/link.component.tsx
+++ src/components/link.component.tsx
@@ -1,14 +1,14 @@
 import { HTMLAttributeAnchorTarget, ReactElement } from "react";
 
 type LinkPropsTypes = {
-  href: string;
-  value: string | ReactElement;
+  href?: string;
+  value?: string | ReactElement;
   className?: string;
   target?: HTMLAttributeAnchorTarget;
 };
 
 function Link(props: LinkPropsTypes) {
-  const { value, target = "_blank", href, className } = props;
+  const { value, target = "_blank", href = "", className } = props;
   return (
     <a
       href={href}
