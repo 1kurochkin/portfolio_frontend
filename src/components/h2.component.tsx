b0e1c8f2a0cce44870a50675769fe52451f2f6a5
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-06 09:04:25 -0500

    create motion h2

--- src/components/h2.component.tsx
+++ src/components/h2.component.tsx
@@ -1,15 +1,21 @@
+import { motion } from "framer-motion";
+import { forwardRef } from "react";
+
 type H2PropsTypes = {
-  text: string;
+  text: string | JSX.Element | Array<JSX.Element> | undefined;
 }
 
-function H2(props: H2PropsTypes) {
+const H2 = forwardRef((props: H2PropsTypes, ref: React.LegacyRef<HTMLHeadingElement>) => {
   const {text} = props;
   return (
-    <h2 className="sm:text-3xl text-3xl border-b-2 pb-2 border-secondary w-fit mb-10 font-bold ">
+    <h2 ref={ref} className="sm:text-3xl text-3xl border-b-2 pb-2 border-secondary w-fit mb-10 font-bold ">
       <span className="text-secondary">~/</span>
       {text}
     </h2>
   );
-}
+})
 
-export default H2;
+const MH2 = motion(H2);
+
+export { H2, MH2 };
+
