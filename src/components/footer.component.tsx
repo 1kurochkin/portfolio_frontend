2ac3aa2f950021a04459e8dc59b864b6fcb9a0a9
Bob Developer <bobdeveloperbob@gmail.com>
2023-02-06 08:59:34 -0500

    add props type sectionId

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -1,13 +1,14 @@
 import Link from "./link.component";
-import Paragraph from "./paragraph.component";
+import {Paragraph} from "./paragraph.component";
 
 type FooterPropsTypes = {
   contactLinks: Array<{ href: string; text: string | undefined }>;
+  sectionId?: string;
 };
 
-function Footer({ contactLinks }: FooterPropsTypes) {
+function Footer({ contactLinks, sectionId }: FooterPropsTypes) {
   return (
-    <footer className="flex bg-inherit border-t-2 border-double px-2 py-5 font-bold">
+    <footer id={sectionId} className="flex bg-inherit border-t-2 border-double px-2 py-5 font-bold">
       <ul className="flex justify-around items-center flex-wrap w-full">
         {contactLinks.map(({ text, href }, index) => (
           <li className="flex justify-center xl:basis-full">
